7eb3fd5a6ec7be58ed50c9b2fa3f3a130c207e19
Neriya Malachi <neriamalachi@gmail.com>
2023-10-22 09:41:51 +0300

    first commit

--- components/Bulb.js
+++ components/Bulb.js
@@ -1,5 +1,19 @@
+
+import Image from "next/image";
+
+
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return (
+    <div className="absolute -left-36 -bottom-12 rotate-12 min-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px] " >
+      <Image
+        src={'/bulb.png'}
+        width={260}
+        height={200}
+        className="w-full h-full"
+        alt="BULB_IMAGE"
+      />
+    </div>
+  );
 };
 
 export default Bulb;
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,27 @@
+
+import Image from "next/image";
+import Link from "next/link";
+import Socials from './Socials'
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px] ">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between  items-center gap-y-6 ">
+          <Link href={'/'}>
+            <Image
+              src={'/logo.png'}
+              width={130}
+              height={58}
+              alt='LOGO IMAGE'
+              priority={true}
+            />
+          </Link>
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
