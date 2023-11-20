9580cd316515e8e828140059133f172e9ef9411c
Neriya Malachi <neriamalachi@gmail.com>
2023-11-20 12:10:43 +0200

    move the nodemailer for api folder

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { mailOptions, transporter } from "../contact/config/nodemailer";
+import { mailOptions, transporter } from "./config/nodemailer";
 
 
 
