4d72e43e8f0d8b58981fde04c622e4ac455b947e
Neriya Malachi <neriamalachi@gmail.com>
2024-07-25 15:28:02 +0300

    .

--- pages/api/config/nodemailer.js
+++ pages/api/config/nodemailer.js
@@ -3,6 +3,7 @@ import nodemailer from "nodemailer"
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
 
+console.log(email,pass);
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
